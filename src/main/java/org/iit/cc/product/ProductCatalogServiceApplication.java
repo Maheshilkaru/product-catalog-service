d89235cf4f965249d3da7f999a8c65c24417752c
Maheshilkaru <maheshilkarunarathna@gmail.com>
2025-04-24 23:12:23 +0530

    Add main class and update pom.xml for product-catalog-service

--- src/main/java/org/iit/cc/product/ProductCatalogServiceApplication.java
+++ src/main/java/org/iit/cc/product/ProductCatalogServiceApplication.java
@@ -1,1 +1,11 @@
 package org.iit.cc.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogServiceApplication.class, args);
+    }
+}
